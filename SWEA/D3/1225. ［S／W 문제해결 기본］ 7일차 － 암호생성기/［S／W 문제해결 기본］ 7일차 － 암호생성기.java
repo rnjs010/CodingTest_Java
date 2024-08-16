8f9af4a0c20a27cbbd9eea472b08ce108b762f90
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-08-16 12:06:22 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 132 ms, Memory: 21,528 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc = 1; tc <= 10; tc++)
+        {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Queue<Integer> q = new LinkedList<>();
+            for (int i = 0; i < 8; i++) {
+            	q.add(Integer.parseInt(st.nextToken()));
+            }
+            
+            int num = 1;
+            boolean chk = true;
+            while(chk) {
+            	for (int j = 1; j < 6; j++) {
+            		int tmp = q.poll() - j;
+            		if (tmp <= 0) {
+            			q.add(0);
+            			chk = false;
+            			break;
+            		} else {
+            			q.add(tmp);
+            		}
+            	}
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i : q) {
+            	if (sb.length() > 0) {
+            		sb.append(" ");
+            	}
+            	sb.append(i);
+            }
+            System.out.println("#" + tc + " " + sb.toString());           
+        }
+    }
+}
