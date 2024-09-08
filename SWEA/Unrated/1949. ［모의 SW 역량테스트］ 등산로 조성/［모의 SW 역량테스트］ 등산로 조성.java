63fc05368eaf1c6f57da569db29a239dbe30da5a
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-09-08 13:41:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 123 ms, Memory: 20,772 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -2,22 +2,21 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int N, K;
-    static int[][] board;
-    static int[][] visited;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int ans;
+    static int N, K, ans;
+    static int[][] board, visit;
+    static int[] dx = {1, 0, -1, 0}, dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            board = new int[N][N];
+
             int high = 0;
+            board = new int[N][N];
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
@@ -26,37 +25,37 @@ class Solution {
                 }
             }
 
-            visited = new int[N][N];
             ans = 0;
+            visit = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (board[i][j] == high) {
-                        visited[i][j] = 1;
+                        visit[i][j] = 1;
                         dfs(i, j, 1, 1);
-                        visited[i][j] = 0;
+                        visit[i][j] = 0;
                     }
                 }
             }
 
-            System.out.println("#" + tc + " " + ans);
+            System.out.println("#" + t + " " + ans);
         }
     }
 
     public static void dfs(int x, int y, int cnt, int cut) {
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d], ny = y + dy[d];
-            if (0 <= nx && nx < N && 0 <= ny && ny < N && visited[nx][ny] == 0) {
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k], ny = y + dy[k];
+            if (0 <= nx && nx < N && 0 <= ny && ny < N && visit[nx][ny] == 0) {
                 if (board[nx][ny] < board[x][y]) {
-                    visited[nx][ny] = 1;
+                    visit[nx][ny] = 1;
                     dfs(nx, ny, cnt + 1, cut);
-                    visited[nx][ny] = 0;
+                    visit[nx][ny] = 0;
                 }
                 else if (cut == 1) {
-                    for (int k = 1; k <= K; k++) {
-                        if (board[nx][ny] - k < board[x][y]) {
-                            board[nx][ny] -= k;
+                    for (int c = 1; c <= K; c++) {
+                        if (board[nx][ny] - c < board[x][y]) {
+                            board[nx][ny] -= c;
                             dfs(nx, ny, cnt + 1, 0);
-                            board[nx][ny] += k;
+                            board[nx][ny] += c;
                         }
                     }
                 }
