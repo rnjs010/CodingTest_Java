46ad4b41af553c2396834ec736c209afbbd1726e
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-07-26 23:20:52 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int len = names.length / 5;
+        if (names.length % 5 != 0) {
+            len += 1;
+        }
+        
+        String[] answer = new String[len];
+        
+        for (int i = 0; i < len; i++) {
+            answer[i] = names[i * 5];
+        }
+        
+        return answer;
+    }
+}
