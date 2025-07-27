f93b4bfbf8ff751c29cefd14f25a318ded7e494c
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-07-27 16:02:02 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 43.90 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        while (true) {
+            int cnt = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] /= 2;
+                } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+                    arr[i] = arr[i] * 2 + 1;
+                } else {
+                    cnt += 1;
+                }
+            }
+            if (cnt == arr.length) {
+                break;
+            } else {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
