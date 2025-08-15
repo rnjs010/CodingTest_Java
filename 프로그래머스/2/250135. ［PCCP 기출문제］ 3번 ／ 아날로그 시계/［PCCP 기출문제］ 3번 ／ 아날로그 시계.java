31f5b5b905479c67a2224af85417e395045a71e3
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-08-15 19:10:20 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 5.91 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,5 +1,5 @@
 class Solution {
-    double curHd, curMd, curSd;
+    static double curHd, curMd, curSd;
     
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
         int ans = 0;
@@ -21,29 +21,24 @@ class Solution {
     }
     
     public void calDegree(int h, int m, int s) {
-        this.curHd = (h % 12)*30 + m*(0.5) + s*((double) 1/120);
-        this.curMd = (m*6) + (s*0.1);
-        this.curSd = s*6;
+        curHd = (h % 12)*30 + m*(0.5) + s*((double) 1/120);
+        curMd = (m*6) + (s*0.1);
+        curSd = s*6;
     }
     
     public int nextD() {
         int cnt = 0;
-        double prevH = this.curHd;
-        double prevM = this.curMd;
-        double prevS = this.curSd;
+        double nextHd = curHd + ((double) 1/120);
+        double nextMd = curMd + 0.1;
+        double nextSd = curSd + 6;
         
-        this.curHd += ((double) 1/120);
-        this.curMd += 0.1;
-        this.curSd += 6;
+        if (Double.compare(curHd, curSd) > 0 && Double.compare(nextHd, nextSd) <= 0) cnt++;
+        if (Double.compare(curMd, curSd) > 0 && Double.compare(nextMd, nextSd) <= 0) cnt++;
+        if ((Double.compare(curHd, curMd) > 0 && Double.compare(curHd, curSd) > 0 && Double.compare(nextHd, nextMd) <= 0) && Double.compare(nextHd, nextSd) <= 0) cnt--;
         
-        if(Double.compare(prevH, prevS) > 0 && Double.compare(curHd,curSd) <= 0) cnt++;
-        if(Double.compare(prevM, prevS) > 0 && Double.compare(curMd,curSd) <= 0) cnt++;
-        if((Double.compare(prevH, prevM) > 0 && Double.compare(prevH,prevS) > 0) 
-           && (Double.compare(curHd, curMd) <= 0 && Double.compare(curHd, curSd) <= 0)) cnt--;
-
-        if(this.curHd >=360) this.curHd %= 360; 
-        if(this.curMd >=360) this.curMd %= 360;
-        if(this.curSd >=360) this.curSd %= 360;
+        curHd = nextHd % 360;
+        curMd = nextMd % 360;
+        curSd = nextSd % 360;
         return cnt;
     }
 }
