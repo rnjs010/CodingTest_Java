89895bc187a5a96220887032ba26fbe4196a38f3
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-10-07 04:27:13 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.39 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,60 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] tDate = changeFormat(today);
-        
+        int tDate = toDays(today);
+
+        // 약관 기간 저장
         Map<String, Integer> term = new HashMap<>();
         for (String str : terms) {
             String[] s = str.split(" ");
             term.put(s[0], Integer.parseInt(s[1]));
         }
-        
+
         ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
             String[] s = privacies[i].split(" ");
-            int[] pDate = changeFormat(s[0]);
-            int mon = pDate[1] + term.get(s[1]);
-            if (mon > 12) {
-                if (mon % 12 == 0) {
-                    pDate[0] += mon / 12 - 1;
-                    mon = 12;
-                } else {
-                    pDate[0] += mon / 12;
-                    mon %= 12;
-                }
-            }
-            pDate[1] = mon;
-            
-            if (tDate[0] > pDate[0]) {
-                ans.add(i+1);
-                continue;
-            } else if (tDate[0] == pDate[0]) {
-                if (tDate[1] > pDate[1]) {
-                    ans.add(i+1);
-                    continue;
-                } else if (tDate[1] == pDate[1]) {
-                    if (tDate[2] >= pDate[2]) {
-                        ans.add(i+1);
-                        continue;
-                    }
-                }
-            }
+            int start = toDays(s[0]);
+            int expire = start + term.get(s[1]) * 28; // 한 달 = 28일 기준
+
+            if (expire <= tDate) ans.add(i + 1);
         }
-        
-        int[] answer = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            answer[i] = ans.get(i).intValue();
-        }
-        return answer;
+
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
-    
-    
-    static int[] changeFormat(String date) {
+
+    // 날짜를 "YYYY.MM.DD" → 총 일수로 변환
+    static int toDays(String date) {
         String[] str = date.split("\\.");
-        int[] res = new int[3];
-        for (int i = 0; i < 3 ; i++) {
-            res[i] = Integer.parseInt(str[i]);
-        }
-        return res;
+        int y = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        int d = Integer.parseInt(str[2]);
+        return (y * 12 * 28) + (m * 28) + d;
     }
 }
