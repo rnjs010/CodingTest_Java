374236068575c372292e27179c4031bffffa843a
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-12-09 00:43:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.64 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,25 +1,14 @@
 class Solution {
-    public String solution(String s) {
-        String str = s.toLowerCase();
-        char[] strArr = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            strArr[i] = str.charAt(i);
+  public String solution(String s) {
+        String answer = "";
+        String[] str = s.toLowerCase().split("");
+        boolean isStart = true;
+
+        for(String ss : str) {
+            answer += isStart ? ss.toUpperCase() : ss;
+            isStart = ss.equals(" ") ? true : false;
         }
-        
-        boolean wordS = true;
-        for (int i = 0; i < s.length(); i++) {
-            if (strArr[i] == ' ') {
-                wordS = true;
-            }
-            else if (Character.isDigit(strArr[i])) {
-                wordS = false;
-            } else if (wordS) {
-                strArr[i]  = Character.toUpperCase(strArr[i]);
-                wordS = false;
-            }
-        }
-        
-        String answer = new String(strArr);
+
         return answer;
-    }
+  }
 }
