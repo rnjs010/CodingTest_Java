663505d90778bcc5368da3f1ac7b2d6fd0429a48
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-12-18 17:53:08 +0900

    [level 2] Title: [1차] 캐시, Time: 37.91 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) return cities.length * 5;
+
         int answer = 0;
-        
-        if (cacheSize == 0) {
-            return cities.length * 5;
-        }
-        
-        List<String> cache = new LinkedList<>();
-        for (String c: cities) {
+
+        Map<String, String> cache = new LinkedHashMap<String, String>(cacheSize, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+                return size() > cacheSize;
+            }
+        };
+
+        for (String c : cities) {
             String city = c.toLowerCase();
-            if (cache.contains(city)) {
-                cache.remove(city);
-                cache.add(city);
+
+            if (cache.containsKey(city)) {
                 answer += 1;
             } else {
-                if (cache.size() >= cacheSize) {
-                    cache.remove(0);   
-                }
-                cache.add(city);
                 answer += 5;
             }
+            cache.put(city, city);
         }
-        
+
         return answer;
     }
 }
