f3ee6aae08f950e847a7ff49c6a6381b83131043
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2025-12-30 00:29:04 +0900

    [level 2] Title: [3차] 압축, Time: 4.00 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> res = new ArrayList<>();
-        
         Map<String, Integer> dict = new HashMap<>();
         for (int i = 0; i < 26; i++) {
-            dict.put(String.valueOf((char) ('A' + i)), i + 1);
+            dict.put(String.valueOf((char)('A' + i)), i + 1);
         }
-        
-        int s = 0, find = 0, last = 27;
-        while (s < msg.length()) {
-            int e = s + 1;
-            
-            while (e <= msg.length() && dict.containsKey(msg.substring(s, e))) {
-                e++;
-            }
-            
-            String w = msg.substring(s, e - 1);
-            res.add(dict.get(w));
-            
-            if (e <= msg.length()) {
-                String wc = msg.substring(s, e);
-                dict.put(wc, last++);
-            }
 
-            s = e - 1;
+        List<Integer> result = new ArrayList<>();
+        int nextIndex = 27;
+
+        StringBuilder w = new StringBuilder();
+        for (int i = 0; i < msg.length(); i++) {
+            w.append(msg.charAt(i));
+
+            if (!dict.containsKey(w.toString())) {
+                String prev = w.substring(0, w.length() - 1);
+                result.add(dict.get(prev));
+
+                dict.put(w.toString(), nextIndex++);
+
+                w.setLength(0);
+                w.append(msg.charAt(i));
+            }
         }
-        
-        return res.stream().mapToInt(i -> i).toArray();
+
+        result.add(dict.get(w.toString()));
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
