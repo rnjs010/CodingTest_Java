a2592ed9b1a56f34771d5763e6a017c4209b6fc7
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-01-02 21:38:15 +0900

    [level 2] Title: 오픈채팅방, Time: 123.25 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,27 +1,49 @@
 import java.util.*;
 
 class Solution {
+
+    private static final String ENTER_MSG = "님이 들어왔습니다.";
+    private static final String LEAVE_MSG = "님이 나갔습니다.";
+
+    private static class Log {
+        String uid;
+        String msg;
+
+        Log(String uid, String msg) {
+            this.uid = uid;
+            this.msg = msg;
+        }
+    }
+
     public String[] solution(String[] record) {
-        Map<String, String> name = new HashMap<>();
-        List<String[]> li = new ArrayList<>();
-        
-        for (String str: record) {
+        Map<String, String> nameMap = new HashMap<>();
+        List<Log> logs = new ArrayList<>(record.length);
+
+        for (String str : record) {
             String[] s = str.split(" ");
-            if (s[0].equals("Change")) {
-                name.put(s[1], s[2]);    
-            } else if (s[0].equals("Enter")) {
-                li.add(new String[] {s[1], "님이 들어왔습니다."});
-                name.put(s[1], s[2]);
-            } else {
-                li.add(new String[] {s[1], "님이 나갔습니다."});
+
+            switch (s[0]) {
+                case "Enter":
+                    nameMap.put(s[1], s[2]);
+                    logs.add(new Log(s[1], ENTER_MSG));
+                    break;
+
+                case "Leave":
+                    logs.add(new Log(s[1], LEAVE_MSG));
+                    break;
+
+                case "Change":
+                    nameMap.put(s[1], s[2]);
+                    break;
             }
         }
-        
-        String[] answer = new String[li.size()];
-        for (int i = 0; i < li.size(); i++) {
-            answer[i] = name.get(li.get(i)[0]) + li.get(i)[1];
+
+        String[] answer = new String[logs.size()];
+        for (int i = 0; i < logs.size(); i++) {
+            Log log = logs.get(i);
+            answer[i] = nameMap.get(log.uid) + log.msg;
         }
-        
+
         return answer;
     }
 }
