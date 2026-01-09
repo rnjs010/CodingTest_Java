f69d4b6e1877efc06cd38bc431f1b6ed732cc0d2
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-01-10 03:02:26 +0900

    [level 2] Title: 시소 짝꿍, Time: 34.25 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -3,44 +3,43 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-
-        Map<Integer, Long> map = new HashMap<>();
-        for (int w : weights) {
-            map.put(w, map.getOrDefault(w, 0L) + 1);
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int w: weights) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
         }
-
+        
         for (long cnt : map.values()) {
             if (cnt >= 2) {
                 answer += cnt * (cnt - 1) / 2;
             }
         }
-
-        for (int w : map.keySet()) {
-            long cnt = map.get(w);
-
+        
+        for (int k: map.keySet()) {
+            long cnt = map.get(k);
+            
             // 2 : 3
-            if (w * 3 % 2 == 0) {
-                int target = w * 3 / 2;
-                if (w < target && map.containsKey(target)) {
+            if (k * 3 % 2 == 0) {
+                int target = k * 3 / 2;
+                if (map.containsKey(target)) {
                     answer += cnt * map.get(target);
                 }
             }
 
-            // 2 : 4
-            int target2 = w * 2;
-            if (w < target2 && map.containsKey(target2)) {
+            // 2 : 4 (즉 1 : 2)
+            int target2 = k * 2;
+            if (map.containsKey(target2)) {
                 answer += cnt * map.get(target2);
             }
 
             // 3 : 4
-            if (w * 4 % 3 == 0) {
-                int target = w * 4 / 3;
-                if (w < target && map.containsKey(target)) {
+            if (k * 4 % 3 == 0) {
+                int target = k * 4 / 3;
+                if (map.containsKey(target)) {
                     answer += cnt * map.get(target);
                 }
             }
         }
-
         return answer;
     }
 }
