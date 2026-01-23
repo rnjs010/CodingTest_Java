e9e99b9e43bd1affd8cf0c2a2e94c605170c43dd
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-01-23 23:46:56 +0900

    [level 2] Title: 무인도 여행, Time: 19.52 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -3,63 +3,49 @@ import java.util.*;
 class Solution {
     int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
     int n, m;
-    char[][] map;
     boolean[][] visit;
-    
+
     public int[] solution(String[] maps) {
         n = maps.length;
         m = maps[0].length();
-        map = new char[n][m];
         visit = new boolean[n][m];
-        
+
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                map[i][j] = maps[i].charAt(j);
-            }
-        }
-        
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visit[i][j] && map[i][j] != 'X') {
-                    System.out.println(i + " " + j + " 값" + map[i][j]);
-                    arr.add(bfs(i, j));
+                if (!visit[i][j] && maps[i].charAt(j) != 'X') {
+                    list.add(bfs(i, j, maps));
                 }
             }
         }
-        
-        int len = arr.size();
-        if (len == 0) return new int[] {-1};
-        
-        Collections.sort(arr);
-        int[] answer = new int[len];
-        for (int i = 0; i < len; i++) {
-            answer[i] = arr.get(i);
-        }
-        
-        return answer;
+
+        if (list.isEmpty()) return new int[]{-1};
+
+        Collections.sort(list);
+        return list.stream().mapToInt(i -> i).toArray();
     }
-    
-    public int bfs(int x, int y) {
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offer(new int[] {x, y});
+
+    int bfs(int x, int y, String[] maps) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{x, y});
         visit[x][y] = true;
-        int cnt = map[x][y] - '0';
-        
-        while (!dq.isEmpty()) {
-            int[] cur = dq.poll();
+
+        int sum = maps[x].charAt(y) - '0';
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
             for (int d = 0; d < 4; d++) {
                 int nx = cur[0] + dx[d];
                 int ny = cur[1] + dy[d];
-                if (0 > nx || nx >= n || 0 > ny || ny >= m) continue;
-                if (visit[nx][ny] || map[nx][ny] == 'X') continue;
-                cnt += map[nx][ny] - '0';
-                dq.offer(new int[] {nx, ny});
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (visit[nx][ny] || maps[nx].charAt(ny) == 'X') continue;
+
                 visit[nx][ny] = true;
-                System.out.println(nx + " " + ny + " " + cnt);
+                sum += maps[nx].charAt(ny) - '0';
+                q.offer(new int[]{nx, ny});
             }
         }
-        
-        return cnt;
+        return sum;
     }
 }
