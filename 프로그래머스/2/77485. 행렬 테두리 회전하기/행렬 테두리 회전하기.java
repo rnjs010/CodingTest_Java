3a98d808225b448254ed56395be6bad16470d488
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-01-25 23:43:06 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 13.04 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -21,49 +21,38 @@ class Solution {
     }
     
     public int rotate(int[] q) {
-        Deque<Integer> dq = new ArrayDeque<>();
         int x1 = q[0] - 1, y1 = q[1] - 1;
         int x2 = q[2] - 1, y2 = q[3] - 1;
-        int minVal = Integer.MAX_VALUE;
-        
-        for (int i = y1; i < y2; i++) {
-            dq.offer(map[x1][i]);
-            minVal = Math.min(minVal, map[x1][i]);
-        }
-        
+        int temp = map[x1][y1];
+        int min = temp;
+
+        // 왼쪽
         for (int i = x1; i < x2; i++) {
-            dq.offer(map[i][y2]);
-            minVal = Math.min(minVal, map[i][y2]);
+            map[i][y1] = map[i + 1][y1];
+            min = Math.min(min, map[i][y1]);
         }
-        
-        for (int i = y2; i > y1; i--) {
-            dq.offer(map[x2][i]);
-            minVal = Math.min(minVal, map[x2][i]);
+
+        // 아래
+        for (int j = y1; j < y2; j++) {
+            map[x2][j] = map[x2][j + 1];
+            min = Math.min(min, map[x2][j]);
         }
-        
+
+        // 오른쪽
         for (int i = x2; i > x1; i--) {
-            dq.offer(map[i][y1]);
-            minVal = Math.min(minVal, map[i][y1]);
+            map[i][y2] = map[i - 1][y2];
+            min = Math.min(min, map[i][y2]);
         }
-        
-        dq.offerFirst(dq.pollLast());
-        
-        for (int i = y1; i < y2; i++) {
-            map[x1][i] = dq.pollFirst();
+
+        // 위
+        for (int j = y2; j > y1 + 1; j--) {
+            map[x1][j] = map[x1][j - 1];
+            min = Math.min(min, map[x1][j]);
         }
+
+        map[x1][y1 + 1] = temp;
+
         
-        for (int i = x1; i < x2; i++) {
-            map[i][y2] = dq.pollFirst();
-        }
-        
-        for (int i = y2; i > y1; i--) {
-            map[x2][i] = dq.pollFirst();
-        }
-        
-        for (int i = x2; i > x1; i--) {
-            map[i][y1] = dq.pollFirst();
-        }
-        
-        return minVal;
+        return min;
     }
 }
