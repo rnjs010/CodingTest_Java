09f09454269a1437747e1151d7663ad4be9da4a7
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-02-08 14:07:18 +0900

    [level 2] Title: 지게차와 크레인, Time: 30.27 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -2,25 +2,21 @@ import java.util.*;
 
 class Solution {
     int n, m;
-    boolean[][] visit;
     char[][] map;
     int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
     
     public int solution(String[] storage, String[] requests) {            
         n = storage.length;
         m = storage[0].length();
-        visit = new boolean[n + 2][m + 2];
         map = new char[n + 2][m + 2];
         
         for (int i = 0; i < n + 2; i++) {
-            Arrays.fill(map[i], 'a');
-            Arrays.fill(visit[i], true);
+            Arrays.fill(map[i], '.');
         }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 map[i + 1][j + 1] = storage[i].charAt(j);
-                visit[i + 1][j + 1] = false;
             }
         }
         
@@ -31,7 +27,7 @@ class Solution {
                 for (int i = 1; i <= n; i++) {
                     for (int j = 1; j <= m; j++) {
                         if (map[i][j] == target) {
-                            visit[i][j] = true;
+                            map[i][j] = '.';
                         }
                     }
                 }
@@ -43,17 +39,17 @@ class Solution {
         int answer = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if(!visit[i][j]) answer++;
+                if(map[i][j] != '.') answer++;
             }
         }
         return answer;
     }
     
     private void bfs(char target) {
-        boolean[][] seen = new boolean[n + 2][m + 2];
+        boolean[][] visit = new boolean[n + 2][m + 2];
         Deque<int[]> dq = new ArrayDeque<>();
         dq.offer(new int[] {0, 0});
-        seen[0][0] = true;
+        visit[0][0] = true;
         
         while (!dq.isEmpty()) {
             int[] cur = dq.poll();
@@ -62,13 +58,13 @@ class Solution {
                 int nx = cur[0] + dx[d];
                 int ny = cur[1] + dy[d];
                 if (0 > nx || (n + 2) <= nx || 0 > ny || (m + 2) <= ny) continue;
-                if (seen[nx][ny]) continue;
+                if (visit[nx][ny]) continue;
                 
-                seen[nx][ny] = true;
-                if (visit[nx][ny]) {
+                visit[nx][ny] = true;
+                if (map[nx][ny] == '.') {
                     dq.offer(new int[] {nx, ny});
                 } else if (map[nx][ny] == target) {
-                    visit[nx][ny] = true;
+                    map[nx][ny] = '.';
                 }
             }
         }
