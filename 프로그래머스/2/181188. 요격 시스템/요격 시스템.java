ae195b27ba9df759bfe34987f990d5239c4a1244
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-02-14 14:48:07 +0900

    [level 2] Title: 요격 시스템, Time: 443.18 ms, Memory: 189 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-        Arrays.sort(targets, (a, b) -> a[1] - b[1]);
+        Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
         int answer = 0;
-        int last = -1 * Integer.MAX_VALUE;
+        int last = Integer.MIN_VALUE;
         for (int[] t: targets) {
             if (t[0] >= last) {
                 answer++;
