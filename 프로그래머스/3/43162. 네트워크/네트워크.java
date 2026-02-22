03b292294d61e775b50389de1094ad9bc07fa5a6
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-02-22 23:09:28 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,24 @@
 class Solution {
-    int n;
-    int[][] computers;
     boolean[] visit;
     
     public int solution(int n, int[][] computers) {
-        this.n = n;
-        this.computers = computers;
         visit = new boolean[n];
         int answer = 0;
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1 && !visit[i]) {
-                    answer++;
-                    dfs(i, j);
-                }
+            if (!visit[i]) {
+                answer++;
+                dfs(i, computers);
             }
         }
         return answer;
     }
     
-    private void dfs(int x, int y) {
+    private void dfs(int x, int[][] map) {
         visit[x] = true;
-        for (int i = 0; i < n; i++) {
-            if (computers[x][i] == 1 && !visit[i]) {
-                dfs(i, x);
+        for (int i = 0; i < map.length; i++) {
+            if (map[x][i] == 1 && !visit[i]) {
+                dfs(i, map);
             }
         }
         return;
