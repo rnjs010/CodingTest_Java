ee7cc2cb3ac33b208e1806ed09edc0b78099a5d1
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-02-27 22:47:28 +0900

    [level 3] Title: 숫자 게임, Time: 120.46 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int answer = 0;
+        int a = 0, b = 0;
+        int size = A.length;
+        while (b < size && a < size) {
+            if (A[a] < B[b]) {
+                answer++;
+                a++;
+                b++;
+            } else {
+                b++;
+            }
+        }
+        
+        return answer;
+    }
+}
