b29aa340337a32f4a8c1c1e28a698220b6c39e71
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-03 22:21:36 +0900

    [level 3] Title: 베스트앨범, Time: 6.28 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
 class Solution {
-    static class Node implements Comparable<Node> {
-        int idx, cnt;
-        
-        public Node(int idx, int cnt) {
+    static class Song {
+        int idx, play;
+
+        Song(int idx, int play) {
             this.idx = idx;
-            this.cnt = cnt;
-        }
-        
-        @Override
-        public int compareTo(Node o) {
-            if (o.cnt == this.cnt) return Integer.compare(this.idx, o.idx);
-            return Integer.compare(o.cnt, this.cnt);
+            this.play = play;
         }
     }
+
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genreCnt = new HashMap<>();
-        Map<String, PriorityQueue<Node>> music = new HashMap<>();
+        Map<String, Integer> totalMap = new HashMap<>();
+        Map<String, List<Song>> songMap = new HashMap<>();
+
         for (int i = 0; i < genres.length; i++) {
-            genreCnt.put(genres[i], genreCnt.getOrDefault(genres[i], 0) + plays[i]);
-            music.putIfAbsent(genres[i], new PriorityQueue<>());
-            music.get(genres[i]).add(new Node(i, plays[i]));
+            totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
+            songMap.putIfAbsent(genres[i], new ArrayList<>());
+            songMap.get(genres[i]).add(new Song(i, plays[i]));
         }
-        
-        List<String> genreList = new ArrayList<>(genreCnt.keySet());
-        genreList.sort((a, b) -> (genreCnt.get(b) - genreCnt.get(a)));
-        
+
+        List<String> genreList = new ArrayList<>(totalMap.keySet());
+        genreList.sort((a, b) -> totalMap.get(b) - totalMap.get(a));
+
         List<Integer> result = new ArrayList<>();
-        for (String g: genreList) {
-            PriorityQueue<Node> pq = music.get(g);
-            int count = 0;
-            while (!pq.isEmpty() && count < 2) {
-                result.add(pq.poll().idx);
-                count++;
+        for (String genre : genreList) {
+            List<Song> list = songMap.get(genre);
+            list.sort((a, b) -> {
+                if (b.play == a.play)
+                    return a.idx - b.idx;
+                return b.play - a.play;
+            });
+
+            for (int i = 0; i < Math.min(2, list.size()); i++) {
+                result.add(list.get(i).idx);
             }
         }
-        
+
         return result.stream().mapToInt(i -> i).toArray();
     }
 }
