dac8d9a2853e4951b357d1bab352f7af9663f354
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-04 13:38:12 +0900

    [level 3] Title: 스티커 모으기(2), Time: 7.31 ms, Memory: 57.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int n = sticker.length;
+        if (n == 1) return sticker[0];
+        
+        int[] dp = new int[n];
+        dp[0] = sticker[0];
+        dp[1] = sticker[0];
+        for (int i = 2; i < n - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + sticker[i]);
+        }
+        
+        int[] dp2 = new int[n];
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        for (int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+        }
+        
+        return Math.max(dp[n - 2], dp2[n - 1]);
+    }
+}
