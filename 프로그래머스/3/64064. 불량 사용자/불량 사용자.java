b2000b9f51de355776e408ad2ec939c94ba4c16b
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-05 21:04:51 +0900

    [level 3] Title: 불량 사용자, Time: 113.85 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    Set<String> result = new HashSet<>();
+    List<String> temp = new ArrayList<>();
+    int len;
+    List<String>[] candidates;
+    
+    public int solution(String[] user_id, String[] banned_id) {
+        len = banned_id.length;
+        candidates = new ArrayList[len];
+        for (int i = 0; i < len; i++) {
+            candidates[i] = new ArrayList<>();
+            for (int j = 0; j < user_id.length; j++) {
+                if (possible(banned_id[i], user_id[j])) {
+                    candidates[i].add(user_id[j]);
+                }
+            }
+        }
+        
+        dfs(0);
+        return result.size();
+    }
+    
+    private boolean possible(String ban, String user) {
+        int len = ban.length();
+        if (len != user.length()) return false;
+        for (int i = 0; i < len; i++) {
+            char cur = ban.charAt(i);
+            if (cur != '*') {
+                if (cur != user.charAt(i)) return false;
+            }
+        }
+        return true;
+    }
+    
+    private void dfs(int depth) {
+        if (depth == len) {
+            List<String> list = new ArrayList<>(temp);
+            Collections.sort(list);
+            StringBuilder sb = new StringBuilder();
+            for (String s: list) {
+                sb.append(s).append("|");
+            }
+            result.add(sb.toString());
+            return;
+        }
+        
+        for (String id: candidates[depth]) {
+            if (temp.contains(id)) continue;
+            temp.add(id);
+            dfs(depth + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
