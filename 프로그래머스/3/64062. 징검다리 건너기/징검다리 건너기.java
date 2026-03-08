b3cc5675180149f7e84845fd85bf065f0f324612
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-08 23:41:27 +0900

    [level 3] Title: 징검다리 건너기, Time: 26.49 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int[] stones, int k) {
+        int left = 1;
+        int right = 0;
+        for (int stone : stones) {
+            right = Math.max(right, stone);
+        }
+
+        int answer = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (canCross(stones, k, mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean canCross(int[] stones, int k, int people) {
+        int count = 0;
+        for (int stone : stones) {
+            if (stone - people < 0) {
+                count++;
+                if (count >= k) {
+                    return false;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return true;
+    }
+}
