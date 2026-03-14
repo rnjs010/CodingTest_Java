828bb139cf0434ca24a4f28211d4e754ece7516e
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-14 16:51:11 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 16.24 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,27 +1,19 @@
 class Solution {
     public long solution(int[] sequence) {
-        int size = sequence.length;
-        long[] a = new long[size]; // 1 -1 1 -1
-        long[] b = new long[size]; // -1 1 -1 1
-        
-        for (int i = 0; i < size; i++) {
-            int num = sequence[i];
-            if (i % 2 == 0) {
-                a[i] = num;
-                b[i] = num * -1;
-            } else {
-                b[i] = num;
-                a[i] = num * -1;
-            }
+        long max = Long.MIN_VALUE;
+
+        long a = 0; // pulse: 1 -1 1 -1
+        long b = 0; // pulse: -1 1 -1 1
+
+        for (int i = 0; i < sequence.length; i++) {
+            int val = (i % 2 == 0) ? sequence[i] : -sequence[i];
+
+            a = Math.max(val, a + val);
+            b = Math.max(-val, b - val);
+
+            max = Math.max(max, Math.max(a, b));
         }
-        
-        long answer = Math.max(a[0], b[0]);
-        for (int i = 1; i < size; i++) {
-            a[i] = Math.max(a[i] + a[i - 1], a[i]);
-            b[i] = Math.max(b[i] + b[i - 1], b[i]);
-            long temp = Math.max(a[i], b[i]);
-            answer = Math.max(answer, temp);
-        }
-        return answer;
+
+        return max;
     }
 }
