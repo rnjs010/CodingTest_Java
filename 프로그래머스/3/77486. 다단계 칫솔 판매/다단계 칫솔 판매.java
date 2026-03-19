d109f89623ce7e1576d76311fc8adc8f5bb3129b
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-19 22:39:05 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 21.79 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int n = enroll.length;
+        
+        Map<String, Integer> map = new HashMap<>();
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            map.put(enroll[i], i);
+            if (referral[i].equals("-")) parent[i] = -1;
+            else parent[i] = map.get(referral[i]);
+        }
+        
+        int[] answer = new int[n];
+        for (int i = 0; i < seller.length; i++) {
+            int cur = map.get(seller[i]);
+            int money = amount[i] * 100;
+            while (cur != -1 && money > 0) {
+                int give = money / 10;
+                answer[cur] += (money - give);
+                money = give;
+                cur = parent[cur];
+            }
+            
+        }
+        
+        return answer;
+    }
+}
