a9e22208f590d22869dbf22ac6a9c02bb9aff351
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-26 13:24:29 +0900

    [level 3] Title: 풍선 터트리기, Time: 25.53 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,21 +1,18 @@
 class Solution {
     public int solution(int[] a) {
         int n = a.length;
-        int[] right = new int[n];
-        int[] left = new int[n];
-        left[0] = a[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = Math.min(left[i - 1], a[i]);
-        }
-        
+        int[] right = new int[n];        
         right[n - 1] = a[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             right[i] = Math.min(right[i + 1], a[i]);
         }
         
         int answer = 0;
+        int left = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            if (!(left[i] < a[i] && right[i] < a[i])) {
+            left = Math.min(left, a[i]);
+            
+            if (left >= a[i] || right[i] >= a[i]) {
                 answer++;
             }
         }
