bc0e09550fbad9b1ae491a3163baba126d329e05
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2026-03-28 20:18:53 +0900

    [level 3] Title: 순위, Time: 11.48 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,3 +1,12 @@
+/*
+  사용 알고리즘 : 플로이드 워셜
+  1) A가 B를 이기고, B가 C를 이기면, A는 C를 이긴다 = 간접 관계를 전부 알아야 함
+     -> 모든 노드 쌍의 관계 (2차원 배열 필요), 중간 노드 (경유지 k)
+  2) n이 작음 = O(N³) 가능
+  
+  DFS도 가능 (이기고 지는 graph 2개 만들어야 함)
+*/
+
 class Solution {
     public int solution(int n, int[][] results) {
         boolean[][] map = new boolean[n + 1][n + 1];
@@ -17,12 +26,11 @@ class Solution {
         
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            int win = 0, lose = 0;
+            int cnt = 0;
             for (int j = 1; j <= n; j++) {
-                if (map[i][j]) win++;
-                if (map[j][i]) lose++;
+                if (map[i][j] || map[j][i]) cnt++;
             }
-            if (win + lose == (n - 1)) answer++;
+            if (cnt == (n - 1)) answer++;
         }
         
         return answer;
